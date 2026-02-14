99abbfab413b80e5d074c6d2ccdd8fe17dc93e8b
Santiago Chavarro <santiagoachm@gmail.com>
2026-02-13 19:18:29 -0500

    empieza belvo apis

--- public/js/app.js
+++
@@ -1,6 +0,0 @@
-const conceptProff = document.querySelector('#conceptProff')
-const bankModal = document.querySelector('#bankConnectModal')
-const myModal = new bootstrap.Modal(bankModal);
-    conceptProff.addEventListener('click',()=>{
-        myModal.show()
-    })
