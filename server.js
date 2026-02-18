d0feae82799d9646417130ae316debc197edbed4
juanka-cyber <geminiprof3@gmail.com>
2026-02-18 11:35:51 -0500

    prometeocambio

--- server.js
+++ server.js
@@ -1,30 +1,50 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const belvo = require('belvo').default;
-
-const client = new belvo(
-    '5adf3fb1-3a21-4c2e-9cdb-201d23999410',
-    'z8Va8oTfNHEDgR*IUTRq#7@E#@L-xD7oeMejVhEviTrxMmmGw@fg2nZCjdyFe9mv',
-    'sandbox'
-);
 
 const app = express();
-const port = 3000;
+const port = 3000
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/api/token', async(req, res) =>{
-    try {
-        const token = await client.widgetToken.create();
-        res.json(token);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Error al crear el token");
-    }
-});
+app.get('/api/test-prometeo', async (req, res) =>{
+        const KEY = "twQ0ZeEfCNgzpPW2zK7n9jQG2dBnl2LtnBDDJAx0ZVu6aBgyyp2Rm5Hu24uZIxzH"
+        let params = new URLSearchParams()
+        params.append('provider','test')
+        params.append('username','12345')
+        params.append('password','gfdsa')
 
-app.listen(port, () => {
-    console.log(`🚀 Servidor listo en http://localhost:${port}`);
-});
+        const respuesta = await fetch('https://banking.sandbox.prometeoapi.com/login/',{
+            method: 'post',
+            headers: {
+            'X-API-Key' : KEY,
+            'accept' : 'application/json',
+            'content-type' : 'application/x-www-form-urlencoded'
+            },
+            body: params
+        })
+        const data = await respuesta.json()
+        
+        console.log('enviando esta llave:', data.key)
+        const getData = await fetch('https://banking.sandbox.prometeoapi.com/account/test/',{
+            method: 'get',
+            headers: {
+                'X-API-Key': KEY,
+                'Key': data.key
+            }
+        })
+        const rawData = await getData.text(); // Lo leemos como texto simple
+        console.log("Respuesta bruta del banco:", rawData); // <--- Esto es clave
+        res.json(data)
+
+        
+    });
+    
+
+
+
+app.listen(port, ()=>{
+    console.log(`servidro dewsplegado http://localhost:${port}`)
+})
