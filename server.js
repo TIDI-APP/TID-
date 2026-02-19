d9d4c9e2699ce38317b366527a9357397fa4af4a
Santiago Chavarro <santiagoachm@gmail.com>
2026-02-18 19:04:17 -0500

    implementacion trae datos prometeo

--- server.js
+++ server.js
@@ -27,19 +27,40 @@ app.get('/api/test-prometeo', async (req, res) =>{
         })
         const data = await respuesta.json()
         
-        console.log('enviando esta llave:', data.key)
-        const getData = await fetch('https://banking.sandbox.prometeoapi.com/account/test/',{
+        console.log('enviando esta llave:', data)
+
+        const urlFinal = `https://banking.sandbox.prometeoapi.com/account/?key=${data.key}`;
+        const getData = await fetch( urlFinal,{
             method: 'get',
             headers: {
-                'X-API-Key': KEY,
-                'Key': data.key
+                'accept':'application/json',
+                'X-API-Key': 'twQ0ZeEfCNgzpPW2zK7n9jQG2dBnl2LtnBDDJAx0ZVu6aBgyyp2Rm5Hu24uZIxzH',
             }
         })
-        const rawData = await getData.text(); // Lo leemos como texto simple
-        console.log("Respuesta bruta del banco:", rawData); // <--- Esto es clave
-        res.json(data)
+        const accountsData = await getData.json();
 
+        console.log('-CUENTAS-');
+        console.log(accountsData.accounts); 
         
+
+        const accountNumber = accountsData.accounts[1].number;
+        const accountCurrency = accountsData.accounts[1].currency;
+
+        const urlMovimientos = `https://banking.sandbox.prometeoapi.com/account/${accountNumber}/movement/?currency=${accountCurrency}&date_start=01/01/2023&date_end=31/12/2025&key=${data.key}`
+        
+        const getMovements = await fetch(urlMovimientos, {
+            method: 'get',
+            headers: {
+                'accept': 'application/json',
+                'X-API-Key': KEY,            
+            }
+        })
+
+        const dataMovements = await getMovements.json();
+
+    
+        console.log('dice prometeo', dataMovements)
+        res.json(dataMovements)
     });
     
 
