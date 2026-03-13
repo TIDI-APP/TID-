8ff3bc1459e863fbe1eae6d6de2115cc9cfaf8d5
TITOS.DEV <cdbt3980@gmail.com>
2026-03-13 00:11:59 -0500

    Corrige callbackURL de Google auth para production

--- src/config/passport.js
+++ src/config/passport.js
@@ -6,7 +6,7 @@ const db = require('../../db/queries');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/auth/google/callback"
 },
     async function (accessToken, refreshToken, profile, cb) {
         try {
